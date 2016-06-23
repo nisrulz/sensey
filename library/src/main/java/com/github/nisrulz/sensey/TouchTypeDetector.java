c7ec2457b31616a00b24d68db5755cf681a1e341
Michael <spitsinmichael@gmail.com>
2016-06-24 00:53:45 +0300

    - fixed onFling in TouchTypeDetector

--- library/src/main/java/com/github/nisrulz/sensey/TouchTypeDetector.java
+++ library/src/main/java/com/github/nisrulz/sensey/TouchTypeDetector.java
@@ -43,11 +43,6 @@ public class TouchTypeDetector {
     private float flingMin = 100;
     private float velocityMin = 100;
 
-    //user will move forward through messages on fling up or left
-    boolean forward = false;
-    //user will move backward through messages on fling down or right
-    boolean backward = false;
-
     @Override public boolean onDoubleTap(MotionEvent e) {
       touchTypListener.onDoubleTap();
       return super.onDoubleTap(e);
@@ -64,7 +59,6 @@ public class TouchTypeDetector {
 
     @Override
     public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-
       //calculate the change in X position within the fling gesture
       float horizontalDiff = e2.getX() - e1.getX();
       //calculate the change in Y position within the fling gesture
@@ -73,30 +67,13 @@ public class TouchTypeDetector {
       float absHDiff = Math.abs(horizontalDiff);
       float absVDiff = Math.abs(verticalDiff);
       float absVelocityX = Math.abs(velocityX);
-      float absVelocityY = Math.abs(velocityY);
 
       if (absHDiff > absVDiff && absHDiff > flingMin && absVelocityX > velocityMin) {
-        //move forward or backward
         if (horizontalDiff > 0) {
-          backward = true;
-        } else if (absVDiff > flingMin && absVelocityY > velocityMin) {
-          if (verticalDiff > 0) {
-            backward = true;
-            forward = false;
-          } else {
-            forward = true;
-            backward = false;
-          }
+          touchTypListener.onSwipeRight();
+        } else {
+          touchTypListener.onSwipeLeft();
         }
-      }
-
-      //user is cycling forward through messages
-      if (forward) {
-        touchTypListener.onSwipeRight();
-      }
-      //user is cycling backwards through messages
-      else if (backward) {
-        touchTypListener.onSwipeLeft();
       }
 
       return true;
