afe0895f538fdc9c9168fcbe90d4dc5e46d1e7d2
Nishant Srivastava <nisrulz@gmail.com>
2016-06-27 12:00:44 +0530

    [*] commented the maven-push plugin in library module [-] removed attribution header as attribution is through commit itself [+] added license header

--- library/src/main/java/com/github/nisrulz/sensey/SensorDetector.java
+++ library/src/main/java/com/github/nisrulz/sensey/SensorDetector.java
@@ -1,17 +1,29 @@
+/*
+ * Copyright (C) 2016 Nishant Srivastava
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.nisrulz.sensey;
 
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 
-/**
- * @author Michael Spitsin
- * @since 2016-06-26
- */
 public abstract class SensorDetector implements SensorEventListener {
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {}
+  @Override public void onSensorChanged(SensorEvent sensorEvent) {
+  }
 
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {}
+  @Override public void onAccuracyChanged(Sensor sensor, int i) {
+  }
 }
