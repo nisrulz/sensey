0f22e684f5f100ab8cfd578dca2f07b4eaae33f7
Nishant Srivastava <nisrulz@gmail.com>
2026-05-28 00:30:21 +0200

    Setup Dokka 2.2.0 documentation generation with versioning plugins
    - Add dokka plugin to version catalog with versioning/kotlin-as-java/android-doc plugins
    - Configure Dokka HTML publication for sensey module with source links
    - Add publishDocs Gradle task for versioned documentation output
    - Add github-pages branch restore step for versioning continuity

--- sensey/build.gradle.kts
+++ sensey/build.gradle.kts
@@ -19,6 +19,8 @@ plugins {
     alias(libs.plugins.compose.compiler)
 
     alias(libs.plugins.maven.publish)
+
+    alias(libs.plugins.dokka)
 }
 
 android {
@@ -64,7 +66,28 @@ android {
     }
 }
 
+val libraryVersion by extra(LibraryInfo.POM_VERSION)
+
+dokka {
+    dokkaPublications.html {
+        moduleName.set(LibraryInfo.POM_NAME)
+        moduleVersion.set(LibraryInfo.POM_VERSION)
+        includes.from("README.md")
+    }
+    dokkaSourceSets.configureEach {
+        sourceLink {
+            localDirectory.set(file("src/main/kotlin"))
+            remoteUrl.set(uri("https://github.com/nisrulz/sensey/tree/master/sensey/src/main/kotlin"))
+            remoteLineSuffix.set("#L")
+        }
+    }
+}
+
 dependencies {
+    dokkaPlugin("org.jetbrains.dokka:versioning-plugin:${libs.versions.dokka.get()}")
+    dokkaPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:${libs.versions.dokka.get()}")
+    dokkaPlugin("org.jetbrains.dokka:android-documentation-plugin:${libs.versions.dokka.get()}")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.annotation)
     implementation(libs.androidx.lifecycle.common)
