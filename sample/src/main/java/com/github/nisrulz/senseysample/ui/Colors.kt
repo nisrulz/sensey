28834609bbfdc90bc1baeef13f5fabce527e06ed
Nishant Srivastava <nisrulz@gmail.com>
2026-05-29 23:14:46 +0200

    Update sample app theme to black background

--- sample/src/main/java/com/github/nisrulz/senseysample/ui/Colors.kt
+++ sample/src/main/java/com/github/nisrulz/senseysample/ui/Colors.kt
@@ -2,7 +2,7 @@ package com.github.nisrulz.senseysample.ui
 
 import androidx.compose.ui.graphics.Color
 
-internal val PrimaryBlue = Color(0xFF35518C)
+internal val PrimaryBlue = Color(0xFF000000)
 internal val AccentOrange = Color(0xFFFF9800)
 internal val NavigationCyan = Color(0xFF16BCD9)
 internal val White = Color(0xFFFFFFFF)
